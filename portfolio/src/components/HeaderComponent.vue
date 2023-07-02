c57c01ab078484a328ac102fcfefd6b2aab93572
jovitif <joaosales911@gmail.com>
2023-07-02 10:11:26 -0300

    header responsivo

--- portfolio/src/components/HeaderComponent.vue
+++ portfolio/src/components/HeaderComponent.vue
@@ -1,30 +1,43 @@
 <template>
     <header>
-        <a class="logo" href="index.html">
-            <img src="../assets/programming-code-signs.png" alt="coding">
-            <h1>Jovitif</h1>
-        </a>
-        <nav class="menu">
-            <ul>
-                <li><a href="#Home">Home</a></li>
-                <li><a href="#About">About</a></li>
-                <li><a href="#Skills">Skills</a></li>
-                <li><a href="#Projects">Projects</a></li>
-            </ul>
-        </nav>
-      
+      <a class="logo" href="index.html">
+        <img src="../assets/programming-code-signs.png" alt="coding">
+        <h1>Jovitif</h1>
+      </a>
+      <div class="menu-toggle" :class="{ active: isMenuOpen }" @click="toggleMenu">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <nav class="menu" :class="{ active: isMenuOpen }">
+        <ul>
+          <li><a href="#Home">Home</a></li>
+          <li><a href="#About">About</a></li>
+          <li><a href="#Skills">Skills</a></li>
+          <li><a href="#Projects">Projects</a></li>
+        </ul>
+      </nav>
     </header>
-</template>
-
-<script>
-export default {
+  </template>
+  
+  <script>
+  export default {
     name: 'HeaderComponent',
-}
-</script>
-
-<style scoped>
-
-header {
+    data() {
+      return {
+        isMenuOpen: false
+      };
+    },
+    methods: {
+      toggleMenu() {
+        this.isMenuOpen = !this.isMenuOpen;
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  header {
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -35,77 +48,100 @@ header {
     top: 0;
     left: 0;
     width: 100%;
-    z-index: 100; 
-}
-
-
-
-.logo {
+    z-index: 100;
+  }
+  
+  .logo {
     display: flex;
     align-items: center;
-}
-
-.logo img {
+  }
+  
+  .logo img {
     margin-right: 10px;
-}
-
-nav ul {
+  }
+  
+  nav ul {
     display: flex;
-}
-
-nav ul li {
+  }
+  
+  nav ul li {
     margin-right: 20px;
-}
-
-@media (max-width: 768px) {
-
+  }
+  
+  @media (max-width: 768px) {
     .menu ul {
-        flex-direction: column;
-        align-items: center;
-        position: absolute;
-        top: 100%;
-        left: 0;
-        background-color: var(--accent-color);
-        visibility:hidden;
-        width: 100%;
-        padding: 1rem 0;
-    
+      flex-direction: column;
+      align-items: center;
+      position: absolute;
+      top: 100%;
+      left: 0;
+      background-color: var(--accent-color);
+      visibility: hidden;
+      width: 100%;
+      padding: 1rem 0;
+      opacity: 0;
+      transition: opacity 0.3s ease;
     }
-
+  
     .menu.active ul {
-        opacity: 1;
-        visibility: visible;
+      opacity: 1;
+      visibility: visible;
     }
-
+  
     .menu li {
-        margin-right: 0;
-        margin-bottom: 1rem;
+      margin-right: 0;
+      margin-bottom: 1rem;
     }
-
+  
     .menu a {
-        color: white;
+      color: white;
     }
-
-    header{
-        display: flex;
-        flex-direction: column;
+  
+    header {
+      display: flex;
+      flex-direction: column;
     }
-   ul{
-        display: flex;
-        padding-top: 10px;
-        flex-direction: column;
+  
+    ul {
+      display: flex;
+      padding-top: 10px;
+      flex-direction: column;
     }
-
-    header{
-        display: flex;
-        flex-direction: column;
+  }
+  
+  .menu-toggle {
+    display: none;
+    cursor: pointer;
+  }
+  
+  @media (max-width: 768px) {
+    .menu-toggle {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      width: 30px;
+      height: 20px;
     }
-   ul{
-        display: flex;
-        padding-top: 10px;
-        flex-direction: column;
+  
+    .menu-toggle span {
+      width: 100%;
+      height: 3px;
+      background-color: white;
+      display: block;
+      transition: all 0.3s ease;
     }
-
-}
-
-</style>
+  
+    .menu-toggle.active span:first-child {
+      transform: rotate(45deg) translate(5px, 5px);
+    }
+  
+    .menu-toggle.active span:nth-child(2) {
+      opacity: 0;
+    }
+  
+    .menu-toggle.active span:last-child {
+      transform: rotate(-45deg) translate(5px, -5px);
+    }
+  }
+  </style>
+  
